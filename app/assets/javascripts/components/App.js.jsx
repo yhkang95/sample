811cefbd8e4661b3c28fc1897f1fd8e055fd55d4
Young H. Kang <young.h.kang.95@gmail.com>
2015-07-18 23:16:52 +0900

    initial commit

---
+++ app/assets/javascripts/components/App.js.jsx
@@ -0,0 +1,14 @@
+var CommentList = require('./CommentList');
+
+module.exports = React.createClass({
+
+  render: function() {
+    return (
+      <div>
+        <h1>Hello world!</h1>
+        <CommentList />
+      </div>
+    );
+  }
+
+});
---
+++ app/assets/javascripts/components/CommentList.js.jsx
@@ -0,0 +1,11 @@
+module.exports = React.createClass({
+
+  render: function() {
+    return (
+      <div>
+        <h1>Comment List</h1>
+      </div>
+    );
+  }
+
+});
